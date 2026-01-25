24685bc2f8af2e18bc7c61d01cdcfd846f8ed87c
kyrachoo <e1136981@u.nus.edu>
2026-01-26 01:01:12 +0800

    Level-0: Renamed "Duke" to Demeter, Greet, Exit

---
+++ src/main/java/Demeter.java
@@ -0,0 +1,6 @@
+public class Demeter {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Demeter.\nWhat can I do for you? \n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
