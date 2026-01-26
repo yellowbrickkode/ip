5b74948646cd3ce9ed9e43ba8e30ca194d81f42b
kyrachoo <e1136981@u.nus.edu>
2026-01-26 10:47:06 +0800

    Level-2: add, list

--- src/main/java/Demeter.java
+++ src/main/java/Demeter.java
@@ -3,13 +3,24 @@ import java.util.Scanner;
 public class Demeter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] lst = new String[100];
+        int idx = 0;
         System.out.println("Hello! I'm Demeter.\nWhat can I do for you? \n");
         while (true) {
             String input = sc.nextLine();
-            System.out.println(input);
+            // System.out.println(input);
 
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < idx; i++) {
+                    int listnumber = i + 1;
+                    System.out.println(listnumber + ". " + lst[i]);
+                }
+            } else {
+                lst[idx] = input;
+                idx += 1;
+                System.out.println("added: " + input);
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
