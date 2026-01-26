393811c2baa3ee323c0bb9c920d1c44fb163e533
kyrachoo <e1136981@u.nus.edu>
2026-01-26 10:32:39 +0800

    Level-1: echoes input, "bye" exits

--- src/main/java/Demeter.java
+++ src/main/java/Demeter.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Demeter {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Demeter.\nWhat can I do for you? \n");
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println(input);
+
+            if (input.equals("bye")) {
+                break;
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
+        sc.close();
     }
 }
