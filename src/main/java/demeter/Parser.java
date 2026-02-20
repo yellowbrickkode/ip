1714a3c627f96ace7ec0a6ab5b1fa623bbc23209
kyrachoo <e1136981@u.nus.edu>
2026-02-20 13:41:46 +0800

    Level-9: Add find command to search tasks by keyword

--- src/main/java/demeter/Parser.java
+++ src/main/java/demeter/Parser.java
@@ -48,6 +48,15 @@ public class Parser {
             String to = input.split("/to")[1].trim();
             Task task = tasks.add(new Event(desc, false, from, to));
             ui.showAdd(task, tasks.size());
+        } else if (input.startsWith("find")) {
+            String keyword = input.substring(5).trim();
+            int count = 1;
+            System.out.println("Here are the matching tasks in your list:");
+            for (int i = 0; i < tasks.size(); i++) {
+                if (tasks.get(i).toString().matches(keyword)) {
+                    System.out.println(count + ". " + tasks.get(i).printTask());
+                }
+            }
 
         } else {
             throw new DemeterExceptions("Sorry, I don't know what you mean.");
